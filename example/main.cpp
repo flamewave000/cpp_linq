92558ccff0854bfe53d5a4014360aa9fee649b89
Andrew F <flamewave000@live.com>
2017-05-02 12:18:51 -0400

    Committing initial example code

---
+++ example/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#define LINQ_USE_MACROS
+#include "../lib/linq.h"
+
+
+using namespace std;
+
+int main() {
+
+	typedef struct{ int i; string desc; } description;
+	int nums[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	// Select the even numbers from the list
+	auto evens = FROM(nums, 10) WHERE { return item % 2 == 0; } ORDERBY(DESCENDING) END;
+	// Select the odd numbers from the list
+	auto odds = linq::from(nums, 10).where([](auto item) { return item % 2 == 1; }).orderby(linq::descending);
+	// Change the data to a new type
+	auto even_descs = FROM(evens) SELECT(description) { return { item, "even" }; } END;
+	auto odd_descs = FROM(odds) SELECT(description) { return { item, "odd" }; } END;
+
+
+	typedef struct { int id, proj_id; string name; } Employee;
+	typedef struct { int id; string name; } Project;
+	typedef struct { struct { int id; string name; } employee; string proj_name; } EmployeeProject;
+	Employee employees[3] = { { 0, 0, "Joe" }, { 1, 1, "Jane" }, { 2, 1, "Alex" } };
+	vector<Project> projects = { { 0, "Financial Inc." }, { 1, "Contracts R Us" } };
+	// First method of joining using the MERGE_JOIN
+	auto empProjs = FROM(employees, 3)
+		MERGE_JOIN(Project, EmployeeProject) projects
+		INTO(EmployeeProject) { return { { left.id, left.name }, right.name }; } // required
+		ON { return left.proj_id == right.id; } // left/right condition
+		END;
+	// Second method of joining using the JOIN to get an array of pairs and then using SELECT to construct the new array
+	empProjs = FROM(employees, 3)
+		JOIN(Project) projects
+		ON { return left.proj_id == right.id; } // left/right condition
+		SELECT(EmployeeProject) { return { { item.left.id, item.left.name }, item.right.name }; }
+		END;
+	// First option as before but without macros
+	empProjs = linq::from(employees, 3)
+		.join<Project, EmployeeProject>(projects,
+			[](auto left, auto right) -> EmployeeProject { return { { left.id, left.name }, right.name }; },
+			[](auto left, auto right) -> bool { return left.proj_id == right.id; }); // left/right condition
+	// Second option as before but without macros
+	empProjs = linq::from(employees, 3)
+		.join<Project>(projects, [](auto left, auto right) -> bool { return left.proj_id == right.id; }) // left/right condition
+		.select<EmployeeProject>([](auto item) ->EmployeeProject { return { { item.left.id, item.left.name }, item.right.name }; });
+
+	return 0;
+}
